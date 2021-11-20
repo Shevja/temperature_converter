7964689444e44cf17749ed6838d57c6ec877e5ac
Shevja <vlsdki@gmail.com>
2021-11-20 17:28:25 +0700

    rubocop correct changes

--- lib/temperature.rb
+++ lib/temperature.rb
@@ -3,6 +3,8 @@ class Temperature
     @source_scale = source_scale
     @target_scale = target_scale
     @value = value
+    @convert_f_to_c = ->(x) { 0.56 * (x - 32) }
+    @convert_k_to_c = ->(x) { x - 273.15 }
   end
 
   def converter
@@ -12,13 +14,13 @@ class Temperature
         'K' => ->(x) { x + 273.15 }
       },
       'F' => {
-        'C' => ->(x) { 0.56 * (x - 32) },
-        'K' => ->(x) { (0.56 * (x - 32)) + 273.15 }
+        'C' => @convert_f_to_c,
+        'K' => ->(x) { @convert_f_to_c.call(x) + 273.15 }
       },
       'K' => {
-        'C' => ->(x) { x - 273.15 },
-        'F' => ->(x) { ((x - 273.15) * 1.8) + 32 }
-      },
+        'C' => @convert_k_to_c,
+        'F' => ->(x) { (@convert_k_to_c.call(x) * 1.8) + 32 }
+      }
     }
     scale_map[@source_scale][@target_scale].call(@value).round(3)
   end
